4c3a445099ab1d3f146c04b1358965b246c0d963
babinicz <babinicz@onet.pl>
2021-04-15 20:04:54 +0200

    zmiana w index

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ import Home from "./components/Home";
 import Discover from "./components/Discover";
 import Footer from "./components/Footer";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import styled from "styled-components";
 
 function App() {
   return (
