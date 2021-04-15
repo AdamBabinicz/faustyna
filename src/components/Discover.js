38f373f5367b8d9dff8242a5e965582598a4a673
babinicz <babinicz@onet.pl>
2021-04-15 13:30:45 +0200

    zmiana margin-top

--- src/components/Discover.js
+++ src/components/Discover.js
@@ -24,7 +24,7 @@ const Discover = () => {
                 {title}
               </h4>
               <p className="mt-4">{desc}</p>
-              <p className="italic text-sm">{par}</p>
+              <p className="italic text-sm mt-4">{par}</p>
             </div>
           );
         })}
