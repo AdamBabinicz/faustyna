36ea582aedbb0a212846e2855bf8a838c3532892
babinicz <babinicz@onet.pl>
2021-04-15 19:14:38 +0200

    dodałem styled

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Home from "./components/Home";
 import Discover from "./components/Discover";
 import Footer from "./components/Footer";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import styled from "styled-components";
 
 function App() {
   return (
